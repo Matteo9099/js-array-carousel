ce34777976efcfe2fc1ce01e7dcc4878f07c1cf1
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-02 17:24:31 +0100

    sistemato carousel

--- main.js
+++ main.js
@@ -1,55 +1,111 @@
-const images = [
-        'img/01.jpg',
-        'img/02.jpg',
-        'img/03.jpg',
-        'img/04.jpg',
-        'img/05.jpg',
+// creo un array contenente le img titoli e testo
+const items = [
+
+    {
+        'photo' : 'img/01.jpg',
+        'title' : 'Svezia',
+        'text' : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+    },
+    {
+        photo: 'img/02.jpg',
+        title: "Svizzera",
+        text: "Lorem ipsum."
+    },
+    {
+        photo: 'img/03.jpg',
+        title: "Gran Bretagna",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        photo: 'img/04.jpg',
+        title: "Germania",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+    },
+    {
+        photo: 'img/05.jpg',
+        title: "Paradise",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+    }
+
 ];
 
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise',
-];
 
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
+// inizializzo variabili
+const itemsRef = document.getElementsByClassName('items')[0];
+const thumbsRef = document.getElementsByClassName('thumbs')[0];
+let item = '';
+let thumb = '';
+let active = 1;
 
 
-let slides = '';
-
-for(let i=0; i<images.length; i++){
-    slides += `<div class="item">
-                    <img src="${images[i]}">
-                </div>`;
+// creo un ciclo for che mi permetta di creare gli elementi nel dom 
+for(let i=0; i<items.length; i++){
+    item += `
+            <div class = "item">
+                <img src="${items[i].photo}" alt="">
+                <div class ="text">
+                    <h3>${items[i].title}</h3>
+                    <p>${items[i].text}</p>
+                </div>
+            </div>`
+    thumb += `
+        <div class = "thumb">
+            <img src ="${items[i].photo}" alt="">
+        </div>`
 }
 
-// creo le slide
-let mostraItem = document.querySelector('.item-container');
-mostraItem.innerHTML = slides;
-let itemView = document.querySelector('.item-view');
-itemView.innerHTML = slides;
+// aggiungo alla classe item la classe active per renderla visibile
+itemsRef.innerHTML = item;
+document.getElementsByClassName('item')[active].classList.add('active');
+
+// aggiungo alla classe thumb la classe active per renderla visibile
+thumbsRef.innerHTML += thumb;
+document.getElementsByClassName('thumb')[active].classList.add('active');
+
+// creo gli eventi al click delle frecce
+const prev = document.querySelector('.prev');
+prev.addEventListener('click', prevClick);
+
+const next = document.querySelector('.next');
+next.addEventListener('click', nextClick);
 
 
-// creo i pallini
-// let circleContainer = document.querySelector('.circle-container');
-// circleContainer.innerHTML = bullets;
 
 
-// inizializzo variabile che mi tenga traccia della slide attiva
-let currentSlide = 0;
 
-let itemsHidden = document.querySelector('.item-container').getElementsByClassName('item');
-let prevItems = document.querySelector('.item-view').getElementsByClassName('item')
-itemsHidden[0].classList.add('active');
-prevItems[0].classList.add('active');
 
-// let circles = document.getElementsByClassName('circle');
-// circles[currentSlide].classList.add('active');
+// creo una funzione al click della freccia 
+function prevClick(){
+    if(active == 0){
+        active = items.length - 1;
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
+    } else if (active < items.length){
+        --active
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
+    }
+}
+
+
+// creo una funzione al click della freccia 
+function nextClick(){
+    if(active < items.length - 1) {
+        ++active
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
+    } else if (active == items.length - 1){
+        active = 0;
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
+    }
+
+}
